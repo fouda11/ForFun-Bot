b7c1dbc8d560d1637ce383e0ac6d7e43a8817ee1
fouda11 <109986212+fouda11@users.noreply.github.com>
2022-07-25 21:39:51 +0200

    init

---
+++ Bot.py
@@ -0,0 +1,31 @@
+import discord
+from discord.ext import commands
+import discord.utils
+
+TOKEN = "MTAwMTE3NjU5OTA2NjA1MDYyMA.GX0AO5.quMFa5e-uM_M-nw5_QVRx8dQSPvC_buZGUl1PA"
+
+bot = commands.Bot(command_prefix="!")
+
+@bot.event
+async def on_ready():
+    print("Ready")
+
+@bot.command(name="give",aliases=["ادي"])
+async def give(ctx, member: discord.Member, role: discord.Role):
+    await member.add_roles(role)
+
+@bot.command(name="remove",aliases=["شيل"])
+async def remove(ctx, member: discord.Member, role: discord.Role):
+    await member.remove_roles(role)
+
+@bot.event
+async def on_raw_reaction_add(payload: discord.RawReactionActionEvent):
+    verify_channel = bot.get_channel(1001184563030143066)
+    if payload.channel_id == verify_channel.id:
+        myguild = bot.get_guild(payload.guild_id)
+        role = discord.utils.get(myguild.roles,name="ForFun")
+        await payload.member.add_roles(role)
+
+
+
+bot.run(TOKEN)
